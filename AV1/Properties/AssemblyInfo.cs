670b1e68b71c36eb8b2003648d4a18cc9204d77c
Felipe Andrade <villela.felipe@al.infnet.edu.br>
2015-11-29 17:25:19 -0200

    restarted new project

--- AV1/Properties/AssemblyInfo.cs
+++ AV1/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("1f7ffc12-169b-49a1-83ce-977e0208ebd3")]
+[assembly: Guid("3459e07c-3956-4c78-9982-f9fe45d0eb5a")]
 
 // Version information for an assembly consists of the following four values:
 //
